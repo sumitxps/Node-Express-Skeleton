bb3c150520b49451af5990c3695a90c50db7ab95
Andy Truong <att35663@uga.edu>
2019-08-27 21:13:59 -0400

    Added modules

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+
+
+const expressLayouts = require("express-ejs-layouts");
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => res.render('index', {layout: false}))
 
 app.get('/test', (req, res) => res.send('Test!'))
 
