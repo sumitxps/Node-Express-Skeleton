96764d90e5dcaa7ae51bf069691e8ee7c12ae102
Andy Truong <att35663@uga.edu>
2019-08-27 21:05:03 -0400

    Test

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("Andy's Tech Tutorials");
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
